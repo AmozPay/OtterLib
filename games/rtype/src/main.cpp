2398645023f0b174ac7e222919bfb23d65c38574
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-14 17:01:27 +0100

    Add: conan file & external dependencies management

--- games/rtype/src/main.cpp
+++ games/rtype/src/main.cpp
@@ -1,5 +1,6 @@
 #include "OtterNetwork.hpp"
 #include <iostream>
+#include <raylib.h>
 
 int main(int ac, char **av) {
 #ifdef TARGET_CLIENT
@@ -7,6 +8,40 @@ int main(int ac, char **av) {
 #elif TARGET_SERVER
     std::cout << "SERVER" << std::endl;
 #endif
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
     Otter::Network::print_hello();
     return 0;
 }
