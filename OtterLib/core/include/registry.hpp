7803b1c4288cab91f7bd10f6c5226d260bf52007
nicolas.saclier@epitech.eu <nicolas.saclier@epitech.eu>
2023-01-17 14:17:30 +0100

    Fix: test + fix for componentnt management

--- OtterLib/core/include/registry.hpp
+++ OtterLib/core/include/registry.hpp
@@ -24,32 +24,35 @@ class ComponentManager {
   public:
     ComponentManager(){};
 
-    template <class C> container_t<C> &register_component() {
-        auto res =
-	  _type_map.try_emplace(std::type_index(typeid(C)), sparse_array<C>());
-        if (res.second == true) {
-	  _destroy_map.emplace(std::type_index(typeid(C)),
-	   [](ComponentManager &m, Entity const &entity) {
-	    m.get_components<C>().erase(entity);
-	  });
-	  std::any resu = (*res.first);
-	  container_t<C> &val = (std::any_cast<container_t<C>>(resu));
-	  return val;
+    template <class C>
+    container_t<C> &register_component() // requires (std::is_default_constructible_v<C>)
+    {
+        auto result =
+            _type_map.try_emplace(std::type_index(typeid(C)), std::make_any<sparse_array<C>>());
+        if (result.second == true) {
+            _destroy_map.emplace(std::type_index(typeid(C)),
+                                 [](ComponentManager &m, Entity const &entity) {
+                                     m.get_components<C>().erase(entity);
+                                 });
         }
-        return std::any_cast<container_t<C>&>(*_type_map.find(std::type_index(typeid(C))));
+        container_t<C> *test =
+            std::any_cast<container_t<C>>(&(_type_map[std::type_index(typeid(C))]));
+        return *test;
     }
 
     template <class C> container_t<C> &get_components() {
-        return std::any_cast<container_t<C>&>(*_type_map.find(std::type_index(typeid(C))));
+        return *(std::any_cast<container_t<C>>(&(_type_map[std::type_index(typeid(C))])));
     }
 
     template <class C> container_t<C> const &get_components() const {
-        return std::any_cast<container_t<C>&>(*_type_map.find(std::type_index(typeid(C))));
+        return *(std::any_cast<container_t<C>>(&(*_type_map.find(std::type_index(typeid(C))))));
     }
 
     template <typename C> reference_type<C> add_component(Entity const &addr, C &&component) {
-        container_t<C> cont = *_type_map.find(std::type_index(typeid(C)));
-        return cont.insert_at(addr, component);
+        container_t<C> &cont = this->get_components<C>();
+
+        std::optional<C> &tmp = cont.insert_at(addr, component);
+        return tmp;
     }
 
     template <typename C, typename... Params>
@@ -57,6 +60,8 @@ class ComponentManager {
         container_t<C> cont = *_type_map.find(std::type_index(typeid(C)));
         return cont.emplace_at(addr, var...);
     }
+    // template <typename C> void remove_component() { rm a full components list
+
     template <typename C> void remove_component(Entity const &addr) {
         container_t<C> cont = *_type_map.find(std::type_index(typeid(C)));
         cont.erase(addr);
