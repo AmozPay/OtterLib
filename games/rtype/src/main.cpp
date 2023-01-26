77bcbdcf4e599c63636df880706133bf1f024114
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-26 22:48:12 +0100

    Feat: update content main game file Split the usage of the graphic libs into the Otterlib

--- games/rtype/src/main.cpp
+++ games/rtype/src/main.cpp
@@ -1,7 +1,8 @@
+#include "OtterGraphic.hpp"
 #include "OtterNetwork.hpp"
 
 #include <iostream>
-#include <raylib.h>
+#include <unistd.h>
 
 int main(int ac, char** av)
 {
@@ -10,40 +11,9 @@ int main(int ac, char** av)
 #elif TARGET_SERVER
     std::cout << "SERVER" << std::endl;
 #endif
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
     Otter::Network::print_hello();
+    Otter::Graphic::start();
+    sleep(10);
+    Otter::Graphic::end();
     return 0;
 }
