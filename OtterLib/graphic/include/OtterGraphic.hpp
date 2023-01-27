ce4cbc69b8f9d2d53715a7b596c59dd9d407c5d3
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-27 22:23:11 +0100

    Feat: add main OtterGraphic include file

---
+++ OtterLib/graphic/include/OtterGraphic.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** RType
+** File description:
+** OtterGraphic.hpp
+*/
+
+#ifndef RTYPE_OTTERGRAPHIC_HPP
+#define RTYPE_OTTERGRAPHIC_HPP
+
+#include "RaylibKeyboard.hpp"
+#include "RaylibMouse.hpp"
+#include "RaylibMusic.hpp"
+#include "RaylibSound.hpp"
+#include "RaylibText.hpp"
+#include "RaylibTexture.hpp"
+#include "RaylibWindow.hpp"
+
+#endif //RTYPE_OTTERGRAPHIC_HPP
