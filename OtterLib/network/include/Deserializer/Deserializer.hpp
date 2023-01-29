411b4785b7a0a3e01bdbd0028c92a0b893cb5191
Baptiste Barbotin <baptiste.barbotin@epitech.eu>
2023-01-29 17:33:23 +0100

    refactor: clean code

--- OtterLib/network/include/Deserializer/Deserializer.hpp
+++ OtterLib/network/include/Deserializer/Deserializer.hpp
@@ -9,25 +9,13 @@
 #define DESERIALIZER_HPP_
 
 #include <boost/archive/binary_iarchive.hpp>
-#include <functional>
 #include <sstream>
-#include <tuple>
-#include <typeindex>
-#include <unordered_map>
-
-using TypeIndexAndIdMap = std::unordered_map<std::type_index, std::size_t>;
-using DeserializeFunction = std::function<void(std::stringbuf&)>;
-using IdAndDeserializeFunctionMap = std::unordered_map<std::size_t, DeserializeFunction>;
 
 namespace Network {
     class Deserializer {
       public:
-        Deserializer();
-        ~Deserializer();
-        void addNewElementToDeserialize(const std::type_index& typeIndex,
-                                        const DeserializeFunction& deserializeFunction);
-        std::size_t getIdOfTypeIndex(const std::type_index& typeIndex) const;
-        void deserializeElement(std::size_t& id, std::stringbuf& stringBuff);
+        Deserializer(){};
+        ~Deserializer(){};
 
         template <class T>
         static T loadArchive(std::stringbuf& stringBuff)
@@ -38,11 +26,6 @@ namespace Network {
             ia >> loadedData;
             return loadedData;
         };
-
-      private:
-        TypeIndexAndIdMap typeIndexAndIdMap;
-        IdAndDeserializeFunctionMap idAndDeserializeFunctionMap;
-        std::size_t id;
     };
 } // namespace Network
 
