cdade14bc9fa86ac47a3086f5198f6b83fda0f19
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-30 11:06:53 +0100

    WIP: add Window system

---
+++ games/rtype/src/system/WindowSystem.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** RType
+** File description:
+** WindowSystem.cpp
+*/
+
+#include "OtterCore.hpp"
+#include "OtterGraphic.hpp"
+
+void OpenWindow(Otter::Core::Orchestrator& ref) {
+    Otter::Graphic::Raylib::RaylibWindow window(800, 600, "RType");
+}
