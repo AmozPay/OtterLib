15b3e50bc6f4337eb8d963cc0e4c16e127cdfc4d
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-31 21:21:59 +0100

    Docs: add Sprite System

--- games/rtype/include/System/SpriteSystem.hpp
+++ games/rtype/include/System/SpriteSystem.hpp
@@ -11,7 +11,21 @@
 #include "OtterCore.hpp"
 
 namespace Otter::Games::RType::System::Sprite {
+
+    /**
+     * @brief Load the sprite system
+     * @details Load the texture with the path for each entity with the texture component
+     * @param ref: The orchestrator
+     * @return void
+     */
     void Load(Otter::Core::Orchestrator& ref);
+
+    /**
+     * @brief Draw the sprite system
+     * @details Draw the texture for each entity with the texture component
+     * @param ref: The orchestrator
+     * @return void
+     */
     void Draw(Otter::Core::Orchestrator& ref);
 } // namespace Otter::Games::RType::System::Sprite
 
