6488434b02705a117750c85c5d0e9679461d5267
Lucas LOUIS <lucas.louis@epitech.eu>
2023-01-31 15:48:37 +0100

    Feat: add main System header file & update include for OtterCore

--- games/rtype/include/System/SpriteSystem.hpp
+++ games/rtype/include/System/SpriteSystem.hpp
@@ -4,4 +4,15 @@
 ** File description:
 ** SpriteSystem.hpp
 */
-#pragma once
+
+#ifndef RTYPE_SPRITESYSTEM_HPP
+#define RTYPE_SPRITESYSTEM_HPP
+
+#include "OtterCore.hpp"
+
+namespace Otter::Games::RType::System::Sprite {
+    void Load(Otter::Core::Orchestrator& ref);
+    void Draw(Otter::Core::Orchestrator& ref);
+} // namespace Otter::Games::RType::System::Sprite
+
+#endif // RTYPE_SPRITESYSTEM_HPP
