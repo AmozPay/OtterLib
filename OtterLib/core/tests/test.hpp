5ee557a9375067f11243f4fa8497f95c9a753912
nicolas.saclier@epitech.eu <nicolas.saclier@epitech.eu>
2023-02-03 16:57:17 +0100

    Tests: entity manager, systemmanager, tested full

--- OtterLib/core/tests/test.hpp
+++ OtterLib/core/tests/test.hpp
@@ -3,7 +3,6 @@
 #include "OtterCore.hpp"
 
 #include <gtest/gtest.h>
-
 class intTest {
   public:
     intTest(int dt) { _dt = dt; }
@@ -33,6 +32,25 @@ class sparse_arrayTest : public ::testing::Test {
     Otter::Core::sparse_array<int> f2;
     Otter::Core::sparse_array<int> f3;
     Otter::Core::sparse_array<intTest> f4;
+};
+
+class SystTest : public ::testing::Test {
+protected:
+  void SetUp() override
+  {
+    s2.registerSystem(([](Otter::Core::Orchestrator& reg){std::cout << "s2.init" << std::endl;}), Otter::Core::SystemManager::Phase::init);
+
+    s3.registerSystem(([](Otter::Core::Orchestrator& reg){std::cout << "s3.init" << std::endl;}), Otter::Core::SystemManager::Phase::init);
+    s3.registerSystem(([](Otter::Core::Orchestrator& reg){std::cout << "s3.update" << std::endl;}), Otter::Core::SystemManager::Phase::update);
+
+    s4.registerSystem(([](Otter::Core::Orchestrator& reg){std::cout << "s4.first";}), Otter::Core::SystemManager::Phase::init);
+    s4.registerSystem(([](Otter::Core::Orchestrator& reg){std::cout << "s4.third";}), Otter::Core::SystemManager::Phase::init);
+s4.registerSystem(([](Otter::Core::Orchestrator& reg){std::cout << "s4.second";}), Otter::Core::SystemManager::Phase::update);
+  }
+  Otter::Core::SystemManager s1;
+  Otter::Core::SystemManager s2;
+  Otter::Core::SystemManager s3;
+  Otter::Core::SystemManager s4;
 };
 
 #endif /* TEST_HPP */
