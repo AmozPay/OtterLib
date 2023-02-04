fc07a80e28e399a5f2caabdaeefee21615f553f6
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-04 16:07:55 +0100

    Feat: add register for Keyboard, Player, Velocity components & Event system

--- games/rtype/src/main.cpp
+++ games/rtype/src/main.cpp
@@ -12,23 +12,30 @@ namespace Otter::Core {
         ref.register_component<Otter::Games::RType::Components::Texture>();
         ref.register_component<Otter::Games::RType::Components::Render>();
         ref.register_component<Otter::Games::RType::Components::Transform>();
+        ref.register_component<Otter::Games::RType::Components::Keyboard>();
+        ref.register_component<Otter::Games::RType::Components::Player>();
+        ref.register_component<Otter::Games::RType::Components::Velocity>();
 
         // TODO: need to be cleaned
         Entity e1 = ref.createEntity();
         ref.add_component(e1, Otter::Games::RType::Components::Window(1280, 720, "title", 60));
 
-//        Otter::Graphic::Raylib::RaylibTexture texture("../assets/image.png");
+        //        Otter::Graphic::Raylib::RaylibTexture texture("../assets/image.png");
         Entity e = ref.createEntity();
-        ref.add_component(e, Otter::Games::RType::Components::Texture("../assets/image.png", Otter::Graphic::Raylib::RaylibTexture("../assets/image.png")));
-        auto test = ref.get_components<Otter::Games::RType::Components::Texture>();
+        ref.add_component(e, Otter::Games::RType::Components::Texture(
+                                 "../assets/image.png", Otter::Graphic::Raylib::RaylibTexture("../assets/image.png")));
         ref.add_component(e, Otter::Games::RType::Components::Render());
         ref.add_component(e, Otter::Games::RType::Components::Transform(0.1, 0, {200, 200}));
+        ref.add_component(e, Otter::Games::RType::Components::Keyboard());
+        ref.add_component(e, Otter::Games::RType::Components::Player(20, "test"));
+        ref.add_component(e, Otter::Games::RType::Components::Velocity(0, {1, 1}));
     }
 
     void registerSystems(Otter::Core::SystemManager& ref)
     {
         ref.registerSystem(Otter::Games::RType::System::Window::SetTargetFPS, Otter::Core::SystemManager::init);
-//        ref.registerSystem(Otter::Games::RType::System::Window::BeginDraw, Otter::Core::SystemManager::preDraw);
+        ref.registerSystem(Otter::Games::RType::System::Event::PollEvent, Otter::Core::SystemManager::preEvent);
+        ref.registerSystem(Otter::Games::RType::System::Window::BeginDraw, Otter::Core::SystemManager::preDraw);
         ref.registerSystem(Otter::Games::RType::System::Window::ClearBackground, Otter::Core::SystemManager::preDraw);
         ref.registerSystem(Otter::Games::RType::System::Sprite::Draw, Otter::Core::SystemManager::draw);
         ref.registerSystem(Otter::Games::RType::System::Window::EndDraw, Otter::Core::SystemManager::subDraw);
