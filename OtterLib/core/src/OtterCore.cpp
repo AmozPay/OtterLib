6b0ed4d44c149c7311db314a0af00d5241776c1b
nicolas.saclier@epitech.eu <nicolas.saclier@epitech.eu>
2023-02-04 18:11:03 +0100

    Add: Fix entity Test

--- OtterLib/core/src/OtterCore.cpp
+++ OtterLib/core/src/OtterCore.cpp
@@ -5,7 +5,8 @@ namespace Otter::Core {
     {
         registerComponents(this->_data);
         registerSystems(this->_systems);
-	createEntityObj(this->_data);
+	 createEntityObj(this->_data);
+				      
         /// call factory
     }
 
