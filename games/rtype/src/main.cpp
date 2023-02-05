c564704aa177ff7d294759c096e575315c410fda
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-05 10:12:52 +0100

    Feat: register Collision system & components

--- games/rtype/src/main.cpp
+++ games/rtype/src/main.cpp
@@ -18,20 +18,33 @@ namespace Otter::Core {
         ref.register_component<Otter::Games::RType::Components::Keyboard>();
         ref.register_component<Otter::Games::RType::Components::Player>();
         ref.register_component<Otter::Games::RType::Components::Velocity>();
+        ref.register_component<Otter::Games::RType::Components::BoxCollider>();
+        ref.register_component<Otter::Games::RType::Components::Health>();
 
         // TODO: need to be cleaned
         Entity e1 = ref.createEntity();
         ref.add_component(e1, Otter::Games::RType::Components::Window(1280, 720, "title", 60));
 
-        //        Otter::Graphic::Raylib::RaylibTexture texture("../assets/image.png");
         Entity e = ref.createEntity();
         ref.add_component(e, Otter::Games::RType::Components::Texture(
                                  "../assets/image.png", Otter::Graphic::Raylib::RaylibTexture("../assets/image.png")));
         ref.add_component(e, Otter::Games::RType::Components::Render());
-        ref.add_component(e, Otter::Games::RType::Components::Transform(0.1, 0, {200, 200}));
+        ref.add_component(e, Otter::Games::RType::Components::Transform(0.05, 0, {200, 410}));
         ref.add_component(e, Otter::Games::RType::Components::Keyboard());
         ref.add_component(e, Otter::Games::RType::Components::Player(20, "test"));
         ref.add_component(e, Otter::Games::RType::Components::Velocity(10, {1, 1}));
+        ref.add_component(e, Otter::Games::RType::Components::BoxCollider(200, 200));
+        ref.add_component(e, Otter::Games::RType::Components::Health(100));
+
+        Entity e2 = ref.createEntity();
+        ref.add_component(e2, Otter::Games::RType::Components::Texture(
+                                  "../assets/image.png", Otter::Graphic::Raylib::RaylibTexture("../assets/image.png")));
+        ref.add_component(e2, Otter::Games::RType::Components::Render());
+        ref.add_component(e2, Otter::Games::RType::Components::Transform(0.05, 0, {200, 200}));
+        ref.add_component(e2, Otter::Games::RType::Components::Keyboard());
+        ref.add_component(e2, Otter::Games::RType::Components::Velocity(10, {1, 1}));
+        ref.add_component(e2, Otter::Games::RType::Components::BoxCollider(200, 200));
+        ref.add_component(e2, Otter::Games::RType::Components::Health(100));
     }
 
     void registerSystems(Otter::Core::SystemManager& ref)
@@ -39,6 +52,7 @@ namespace Otter::Core {
         ref.registerSystem(Otter::Games::RType::System::Window::SetTargetFPS, Otter::Core::SystemManager::init);
         ref.registerSystem(Otter::Games::RType::System::Event::PollEvent, Otter::Core::SystemManager::preEvent);
         ref.registerSystem(Otter::Games::RType::System::Move::EntityMovement, Otter::Core::SystemManager::update);
+        ref.registerSystem(Otter::Games::RType::System::Collision::EntityCollision, Otter::Core::SystemManager::update);
         ref.registerSystem(Otter::Games::RType::System::Window::BeginDraw, Otter::Core::SystemManager::preDraw);
         ref.registerSystem(Otter::Games::RType::System::Window::ClearBackground, Otter::Core::SystemManager::preDraw);
         ref.registerSystem(Otter::Games::RType::System::Sprite::Draw, Otter::Core::SystemManager::draw);
