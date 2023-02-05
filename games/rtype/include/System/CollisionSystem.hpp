467a6b26f7dcaaff279ee71374a6a9074a26297e
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-05 10:10:32 +0100

    Feat: update collision system by optimizing it

--- games/rtype/include/System/CollisionSystem.hpp
+++ games/rtype/include/System/CollisionSystem.hpp
@@ -2,20 +2,34 @@
 ** EPITECH PROJECT, 2023
 ** RType
 ** File description:
-** SpriteSystem.hpp
+** CollisionSystem.hpp
 */
 
-#ifndef RTYPE_SPRITESYSTEM_HPP
-#define RTYPE_SPRITESYSTEM_HPP
+#ifndef RTYPE_COLLISIONSYSTEM_HPP
+#define RTYPE_COLLISIONSYSTEM_HPP
 
 #include "Components.hpp"
 #include "OtterCore.hpp"
-#include "RaylibTexture.hpp"
 
 namespace Otter::Games::RType::System::Collision {
+
+    /**
+     * @brief System for the collision of the entities
+     * @details The collision system is used to check if the entities are colliding with each other
+     * @param ref: The orchestrator
+     * @return void
+     */
     void EntityCollision(Otter::Core::Orchestrator& ref);
 
-    enum CollisionDirection { TOP, BOTTOM, LEFT, RIGHT };
+    /**
+     * @brief Apply changes due to collision
+     * @details If the entity is a player, the health is decreased to 0 if the collision is on the right. Otherwise, the
+     * position is reset to the last checkpoint
+     * @param health: The health of the entity
+     * @param transform: The transform of the entity
+     * @return void
+     */
+    void ApplyChange(auto& health, auto& transform);
 } // namespace Otter::Games::RType::System::Collision
 
-#endif // RTYPE_SPRITESYSTEM_HPP
+#endif // RTYPE_COLLISIONSYSTEM_HPP
--- games/rtype/src/System/CollisionSystem.cpp
+++ games/rtype/src/System/CollisionSystem.cpp
@@ -9,71 +9,49 @@
 
 namespace Otter::Games::RType::System::Collision {
 
-    CollisionDirection collisionDirection(const Otter::Games::RType::Components::BoxCollider col1,
-                                          const Otter::Games::RType::Components::BoxCollider col2,
-                                          const Otter::Games::RType::Components::Transform trans1,
-                                          const Otter::Games::RType::Components::Transform trans2)
+    void ApplyChange(auto& health, auto& transform)
     {
-        if (trans1._position.x + col1.width < trans2._position.x + col2.width &&
-            trans1._position.x + col1.width > trans2._position.x) {
-            return CollisionDirection::RIGHT;
-        } else if (trans1._position.x < trans2._position.x + col2.width && trans1._position.x > trans2._position.x) {
-            return CollisionDirection::LEFT;
-        } else if (trans1._position.y < trans2._position.y + col2.height && trans1._position.y > trans2._position.y) {
-            return CollisionDirection::TOP;
-        } else {
-            return CollisionDirection::BOTTOM;
-        }
-    }
-
-    void apllyChange(CollisionDirection direction, Otter::Games::RType::Components::Player player,
-                     Otter::Games::RType::Components::Health health, Otter::Games::RType::Components::Transform trans,
-                     Otter::Games::RType::Components::BoxCollider col2,
-                     Otter::Games::RType::Components::Transform trans2)
-    {
-        if (direction == RIGHT) {
-            health.hp = 0;
+        // The entity is moving to the right
+        if (transform->_position.x - transform->_lastPosition.x > 0) {
+            health->_hp = 0;
+            std::cout << "RIGHT COLLISION" << std::endl;
             // reset pos
-        } else if (direction == LEFT) {
-            trans._position.x = trans2._position.x + col2.width;
-        } else if (direction == TOP) {
-            trans._position.y = trans2._position.y + col2.height;
-        } else {
-            trans._position.y = trans2._position.y - col2.height;
         }
+        transform->_position = transform->_lastPosition;
     }
 
     void EntityCollision(Otter::Core::Orchestrator& ref)
     {
         auto const& collisions = ref.get_components<Otter::Games::RType::Components::BoxCollider>();
-        auto const& player = ref.get_components<Otter::Games::RType::Components::Player>();
-        auto const& transform = ref.get_components<Otter::Games::RType::Components::Transform>();
-        auto const& texture = ref.get_components<Otter::Games::RType::Components::Texture>();
-        auto const& health = ref.get_components<Otter::Games::RType::Components::Health>();
+        auto const& players = ref.get_components<Otter::Games::RType::Components::Player>();
+        auto& transforms = ref.get_components<Otter::Games::RType::Components::Transform>();
+        auto const& textures = ref.get_components<Otter::Games::RType::Components::Texture>();
+        auto& healths = ref.get_components<Otter::Games::RType::Components::Health>();
 
-        for (size_t i = 0; i < collisions.size(); i++) {
-            for (size_t j = 0; j < collisions.size(); j++) {
-                auto const& colI = collisions[i];
-                auto const& colJ = collisions[j];
-                auto const& transI = transform[i];
-                auto const& transJ = transform[j];
-                auto const& playerI = player[i];
-                auto const& playerJ = player[j];
-                auto const& texI = texture[i];
-                auto const& texJ = texture[j];
-                auto const& helthI = health[i];
-                auto const& helthJ = health[j];
-                if (transI && transJ && colI && colJ && texI && texJ) {
-                    if (playerI || playerJ) {
-                        bool collision = texI->_texture.checkCollisionRecs(
-                            {transI->_position.x, transI->_position.y, colI->width, colI->height},
-                            {transJ->_position.x, transJ->_position.y, colJ->width, colJ->height});
+        for (size_t i = 0; i < collisions.size() && i < transforms.size() && i < textures.size(); i++) {
+            auto const& collisionFirst = collisions[i];
+            auto& transformFirst = transforms[i];
+            auto const& textureFirst = textures[i];
+            if (!collisionFirst || !transformFirst || !textureFirst)
+                continue;
+            for (size_t j = 0; j < collisions.size() && j < transforms.size() && j < textures.size(); j++) {
+                if (j == i)
+                    continue;
+                auto const& collisionsSecond = collisions[j];
+                auto& transformSecond = transforms[j];
+                auto const& playerFirst = players[i];
+                auto const& textureSecond = textures[j];
+                auto& healthFirst = healths[i];
+                if (collisionsSecond && transformSecond && textureSecond) {
+                    if (playerFirst) {
+                        bool collision = textureFirst->_texture.checkCollisionRecs(
+                            {transformFirst->_position.x, transformFirst->_position.y, collisionFirst->_width,
+                             collisionFirst->_height},
+                            {transformSecond->_position.x, transformSecond->_position.y, collisionsSecond->_width,
+                             collisionsSecond->_height});
                         if (collision) {
-                            CollisionDirection side = collisionDirection(*colI, *colJ, *transI, *transJ);
-                            if (playerI) {
-                                apllyChange(side, *playerI, *helthI, *transI, *colJ, *transJ);
-                            } else {
-                                apllyChange(side, *playerJ, *helthJ, *transJ, *colI, *transI);
+                            if (playerFirst) {
+                                ApplyChange(healthFirst, transformFirst);
                             }
                         }
                     }
