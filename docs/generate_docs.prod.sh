a2a939bb7ee84554b4d101b292ed08b5dcba0993
AmozPay <amoz.pay@epitech.eu>
2023-02-06 14:07:23 +0100

    Fix: docs generation

--- docs/generate_docs.prod.sh
+++ docs/generate_docs.prod.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 export OTTERLIB_VERSION=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
 echo $OTTERLIB_VERSION
-doxygen Doxyfile
+(
+    cd doxygen/doxygen-awesome-css
+    git checkout v2.1.0
+)
+doxygen doxygen/Doxyfile
+(
+    cd rfc
+    ./build_rfc.sh
+)
 rm -rf .retype
-mkdir .retype
+mkdir -p .retype/rfc
 mv html .retype/api-reference
+mv rfc/rfc.html .retype/rfc/index.html
+
 yarn && yarn retype build retype.prod.yml
