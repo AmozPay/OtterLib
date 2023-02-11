27752daa857cb07edf89e97ab47d0c2c846a6098
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-11 15:42:32 +0100

    feat: add new draw method for background sprites

--- games/rtype/src/System/SpriteSystem.cpp
+++ games/rtype/src/System/SpriteSystem.cpp
@@ -10,13 +10,22 @@
 
 namespace Otter::Games::RType::System::Sprite {
 
-    void Load(Otter::Core::Orchestrator& ref)
+    void DrawParallax(Otter::Core::Orchestrator& ref)
     {
-        auto const& sprites = ref.get_components<Otter::Games::RType::Components::Texture>();
-        for (size_t i = 0; i < sprites.size(); i++) {
-            auto const& sprite = sprites[i];
-            if (sprite) {
-                //                sprite->_texture = Otter::Graphic::Raylib::RaylibTexture(sprite->_path);
+        auto& sprites = ref.get_components<Otter::Games::RType::Components::Texture>();
+        auto const& transforms = ref.get_components<Otter::Games::RType::Components::Transform>();
+        auto const& renders = ref.get_components<Otter::Games::RType::Components::Render>();
+        auto const& parallaxes = ref.get_components<Otter::Games::RType::Components::Parallax>();
+        for (size_t i = 0; i < sprites.size() && i < transforms.size() && i < renders.size() && i < parallaxes.size();
+             i++) {
+            auto& sprite = sprites[i];
+            auto const& transform = transforms[i];
+            auto const& render = renders[i];
+            auto const& parallax = parallaxes[i];
+            if (sprite && transform && render && parallax) {
+                sprite->_texture.setScale(transform->_scale);
+                sprite->_texture.setRotation(transform->_rotation);
+                sprite->_texture.draw({transform->_position.x, transform->_position.y});
             }
         }
     }
@@ -26,11 +35,13 @@ namespace Otter::Games::RType::System::Sprite {
         auto& sprites = ref.get_components<Otter::Games::RType::Components::Texture>();
         auto const& transforms = ref.get_components<Otter::Games::RType::Components::Transform>();
         auto const& renders = ref.get_components<Otter::Games::RType::Components::Render>();
+        auto const& parallaxes = ref.get_components<Otter::Games::RType::Components::Parallax>();
         for (size_t i = 0; i < sprites.size() && i < transforms.size() && i < renders.size(); i++) {
             auto& sprite = sprites[i];
             auto const& transform = transforms[i];
             auto const& render = renders[i];
-            if (sprite && transform && render) {
+            auto const& parallax = parallaxes[i];
+            if (sprite && transform && render && !parallax) {
                 sprite->_texture.setScale(transform->_scale);
                 sprite->_texture.setRotation(transform->_rotation);
                 sprite->_texture.draw({transform->_position.x, transform->_position.y});
