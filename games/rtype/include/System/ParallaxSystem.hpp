0f0ddee15c2d113d29c9bb73811c79043b3ac517
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-13 14:41:03 +0100

    docs: add documentation for parallax handler system

--- games/rtype/include/System/ParallaxSystem.hpp
+++ games/rtype/include/System/ParallaxSystem.hpp
@@ -13,8 +13,15 @@
 
 namespace Otter::Games::RType::System::Parallax {
 
-    void ParallaxMovement(Otter::Core::Orchestrator& ref);
+    /**
+     * @brief System for the handling of the parallax
+     * @details The parallax system is used to move the entities based on their speed, acceleration and direction. When
+     * the entity is out of the screen, it is moved to the other side
+     * @param ref: The orchestrator
+     * @return void
+     */
+    void ParallaxHandler(Otter::Core::Orchestrator& ref);
 
-}
+} // namespace Otter::Games::RType::System::Parallax
 
 #endif // RTYPE_PARALLAXSYSTEM_HPP
