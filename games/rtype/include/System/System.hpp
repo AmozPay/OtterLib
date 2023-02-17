ce16571202a0250b08063a818cb0d92be0ef5e62
Lucas LOUIS <lucas.louis@epitech.eu>
2023-02-17 21:19:14 +0100

    feat: update system headers

--- games/rtype/include/System/System.hpp
+++ games/rtype/include/System/System.hpp
@@ -8,12 +8,19 @@
 #ifndef RTYPE_SYSTEM_HPP
 #define RTYPE_SYSTEM_HPP
 
+// Collision
 #include "CollisionSystem.hpp"
 #include "DispawnableSystem.hpp"
+#include "EventCollisionSystem.hpp"
+
+// Other
+#include "DeathSystem.hpp"
+#include "EventHandlerSystem.hpp"
 #include "EventNetworkSystem.hpp"
 #include "EventSystem.hpp"
 #include "MoveSystem.hpp"
 #include "ParallaxSystem.hpp"
+#include "PlayerCollisionSystem.hpp"
 #include "SpriteSystem.hpp"
 #include "WindowSystem.hpp"
 
