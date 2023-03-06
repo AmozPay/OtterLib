de2564928c3914c104a4c3853fdcf581bb33f117
Lucas LOUIS <lucas.louis@epitech.eu>
2023-03-06 22:24:26 +0100

    feat: add init random powerup in game

--- games/rtype/src/System/InitGameSystem.cpp
+++ games/rtype/src/System/InitGameSystem.cpp
@@ -10,18 +10,38 @@
 #include "InitEnemy.hpp"
 #include "InitInvisibleWall.hpp"
 #include "InitMobs.hpp"
-#include "InitObstacle.hpp"
+#include "InitPowerup.hpp"
 #include "InitParallaxes.hpp"
 #include "InitPlayer.hpp"
 
 namespace Otter::Games::RType::System::InitGame {
+
+    namespace componets = Otter::Games::RType::Components;
+
+    void CreatePowerup(Otter::Core::Orchestrator& ref, Otter::Core::Entity baseEntity)
+    {
+        int powerupValue[] = {25, 15, 5, 2, 2, 1};
+
+        int powerupMaxNb = 10;
+        int powerupMaxPos = powerupMaxNb * 800;
+        int range = 21 - 1;
+        int num = 0;
+        int powerupType = 0;
+        for (int i = 0; i < powerupMaxNb; i++) {
+            powerupMaxPos = powerupMaxPos - 800;
+            num = rand() % range;
+            powerupType = rand() % 5;
+            Otter::Games::GameClient::Init::InitPowerup powerup(
+                ref, baseEntity, "Enemy " + std::to_string(i),
+                Otter::Games::RType::Utils::Vector2(1280 + powerupMaxPos, 34 * num), static_cast<components::PowerupType>(powerupType), powerupValue[powerupType]);
+        }
+    }
 
     void InitGame(Otter::Core::Orchestrator& ref, Otter::Core::Entity baseEntity)
     {
         Otter::Games::GameClient::Init::InitParallaxes parallaxes(ref, baseEntity);
         Otter::Games::GameClient::Init::InitInvisibleWall invisibleWall(ref, baseEntity);
         Otter::Games::GameClient::Init::InitPlayer player(ref, baseEntity);
-        Otter::Games::GameClient::Init::InitObstacle obstacles(ref, baseEntity);
 
         int enmiesMaxNb = 20;
         int enemyMaxPos = enmiesMaxNb * 300;
@@ -34,6 +54,8 @@ namespace Otter::Games::RType::System::InitGame {
                 ref, baseEntity, "Enemy " + std::to_string(i),
                 Otter::Games::RType::Utils::Vector2(800 + enemyMaxPos, 34 * num));
         }
+
+        CreatePowerup(ref, baseEntity);
     }
 
     void HandleInitGame(Otter::Core::Orchestrator& ref, std::vector<std::size_t>&)
