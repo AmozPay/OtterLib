2700629cc159713600b65830efb31bc82b8c120e
Lucas LOUIS <lucas.louis@epitech.eu>
2023-03-07 00:02:43 +0100

    feat: add multi textures management for power up

--- games/rtype/src/Init/InitPowerup.cpp
+++ games/rtype/src/Init/InitPowerup.cpp
@@ -15,16 +15,18 @@ namespace Otter::Games::GameClient::Init {
 
         auto& textureStorages = ref.get_components<Otter::Core::BaseComponents::TextureStorage>();
 
+        std::string paths[] = {"../assets/power-up-health-20x20.png", "../assets/power-up-damage-20x20.png", "../assets/power-up-speed-20x20.png", "../assets/power-up-slowness-20x20.png", "../assets/power-up-strength-20x20.png", "../assets/power-up-weakness-20x20.png"};
+
         // Powerup generation
         ref.add_component(powerup, Otter::Core::BaseComponents::Texture(
-                                        "../assets/obstacle.gif",
-                                        textureStorages[baseEntity]->findTextureByPath("../assets/obstacle.gif"),
-                                        Otter::Games::RType::Utils::Rectangle(0, 0, 62, 24)));
+                                        paths[powerupType],
+                                        textureStorages[baseEntity]->findTextureByPath(paths[powerupType]),
+                                        Otter::Games::RType::Utils::Rectangle(0, 0, 20, 20)));
         ref.add_component(powerup, Otter::Core::BaseComponents::Render());
 
-        ref.add_component(powerup, Otter::Core::BaseComponents::Transform(1, 0, {pos.x, pos.y}));
+        ref.add_component(powerup, Otter::Core::BaseComponents::Transform(2, 0, {pos.x, pos.y}));
         ref.add_component(powerup, Otter::Core::BaseComponents::Velocity(1.5, 2, {-1, 0}, {0, 0}));
-        ref.add_component(powerup, Otter::Core::BaseComponents::BoxCollider(62, 24));
+        ref.add_component(powerup, Otter::Core::BaseComponents::BoxCollider(40, 40));
 
         ref.add_component(powerup, components::Powerup(powerupType, powerupValue));
         ref.add_component(powerup, components::Obstacle(components::ObstacleType::POWERUP, id));
